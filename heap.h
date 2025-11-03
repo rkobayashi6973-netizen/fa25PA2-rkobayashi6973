5a8bbde47ff894b308bf195490b124a00d017c67
Ryoka Kobayashi <rkobayashi6973@sdsu.edu>
2025-11-03 15:57:34 -0800

    push and pop

--- heap.h
+++ heap.h
@@ -16,12 +16,22 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+        data[size] = idx; // add the index at end of heap
+        upheap(size, weightArr); // restore order using upheap()
+        size++;
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        if(size == 0) {
+            return -1;
+        }
+        int smallest = data[0]; // root is the smallest
+        data[0] = data[size-1]; // replace root with last elemen
+        size--;
+        downheap(0, weightArr);
+        return smallest; //return smallest index
     }
 
     void upheap(int pos, int weightArr[]) {
